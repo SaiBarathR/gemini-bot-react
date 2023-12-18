ee61d81d5d4f3c01bdf723951a0e40de14d6305d
Sai Barath <saibarathdeep@outlook.com>
2023-12-18 12:28:20 +0530

    Add Vite configuration file

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 3000
+  },
+  define: {
+    global: 'window',
+  },
+  build: {
+    outDir: "build"
+  },
+  base: '/',
+})
