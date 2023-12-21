17a3c4047b9dc818a73170b99bbe097a6b6d3afa
Sai Barath <saibarathr@gmail.com>
2023-12-21 12:54:04 +0530

    Remove console.log and localStorage.setItem in config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,4 @@
 const env = import.meta.env.MODE
-console.log('env', env)
-localStorage.setItem('API_KEY', import.meta.env.VITE_APP_BOT_API_KEY)
 export const config = {
     "API_KEY": env === "development" ? "" : import.meta.env.VITE_APP_BOT_API_KEY,
 }
