f517ecf089429b4dcac78ae1f6e78d693de9d12d
Sai Barath <saibarathr@gmail.com>
2023-12-21 12:49:12 +0530

    Update deployment workflow and Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -11,9 +10,6 @@ export default defineConfig({
   },
   define: {
     global: 'window',
-  },
-  build: {
-    outDir: "build"
   },
   base: '/gemini-bot-react/'
 })
