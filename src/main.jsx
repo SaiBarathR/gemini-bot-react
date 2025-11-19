d325abe9156bfd7b153a8a4a4add41e3a99914cc
Sai Barath <saibarathr@gmail.com>
2025-11-19 13:29:58 +0000

    refactor: Replace Chakra UI with a custom theme system and update scrollbar styling.

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { ChakraProvider, ColorModeScript, theme } from '@chakra-ui/react'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>,
 )
